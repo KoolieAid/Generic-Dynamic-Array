94cf0cf548af1e6550ae6ec1003c2a8ddc7af891
Erik <erkgo.eg@gmail.com>
2022-05-02 20:54:52 +0800

    Create DynamicArray.h

---
+++ DynamicArray.h
@@ -0,0 +1,18 @@
+#ifndef DYNA_ARR_H
+#define DYNA_ARR_H
+
+struct DynamicArray;
+
+typedef struct DynamicArray DynamicArray;
+
+DynamicArray* create_dynamic_array(int initialSize);
+
+void destroy_dynamic_array(DynamicArray* a);
+
+int dynamic_array_add_element(DynamicArray* a, void* element);
+
+int dynamic_array_remove_element(DynamicArray* a, int index);
+
+int dynamic_array_free_contents(DynamicArray* a);
+
+#endif
